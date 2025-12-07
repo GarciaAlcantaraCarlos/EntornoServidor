aaef32c2628cc745c7737b90dbb4d45c84ef49b5
GarciaAlcantaraCarlos <garciaalcantaracarlos001.alu@stemgranada.com>
2025-12-07 22:20:38 +0100

    [ FEAT ] Blade frontend is connected + default middleware

--- PHP/UD 3/CRUD_Peliculas_Ultimate/routes/web.php
+++ PHP/UD 3/CRUD_Peliculas_Ultimate/routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('films/list');
+})->middleware('auth');
+
+
+# AUTH
+Route::get('/login', function() {
+    return view('auth/login');
 });
+
+Auth::routes();
+
+
+# CONTENT
+Route::get('/film/detail/{id}', function ( $id ) {
+    return view('films/detail', [ 'id' => $id ]);
+})->middleware('auth');
+
+Route::get('/film/create', function () {
+    return view('films/create');
+})->middleware('admin');
+
+Route::get('/film/delete/{id}', function ( $id ) {
+    return view('films/delete', [ 'id' => $id ]);
+})->middleware('admin');
+
+Route::get('/comment/create/{film_id}', function ( $film_id ) {
+    return view('comments/create', [ 'film_id' => $film_id ]);
+})->middleware('auth');
+
+Route::get('/comment/delete/{id}', function ( $id ) {
+    return view('comments/delete', [ 'id' => $id ]);
+})->middleware('admin');
+
+
+# DEFAULTS
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
